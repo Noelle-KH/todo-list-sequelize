e42a8ef836619fc0492df461169cb909de751a45
Noelle-KH <noelletang86@gmail.com>
2023-02-20 16:09:52 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
