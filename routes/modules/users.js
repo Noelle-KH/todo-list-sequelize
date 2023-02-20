b689c025965cc492fc7836a7ef80ab317cd4e852
Noelle-KH <noelletang86@gmail.com>
2023-02-20 20:23:38 +0800

    feat: add register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const { User } = require('../../models')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -6,6 +8,25 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    if (password !== confirmPassword) {
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+
+    const user = await User.findOne({ where: { email } })
+    if (user) {
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+    
+    await User.create({ name, email, password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)) })
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
