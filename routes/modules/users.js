3486eb70e637155891f7635aac3c18834b24931e
Noelle-KH <noelletang86@gmail.com>
2023-02-22 14:00:46 +0800

    feat: add logout and modify register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+const { v4: uuidv4 } = require('uuid')
 const { User } = require('../../models')
 
 router.get('/login', (req, res) => {
@@ -24,34 +25,48 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  let errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'All fields are required.' })
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ message: 'Password and confirm password not the same.' })
+  }
+
+  if (errors.length) {
+    return res.render('register', { name, email, password, confirmPassword, errors })
+  }
+
   try {
-    const { name, email, password, confirmPassword } = req.body
-    let errors = []
-
-    if (!name || !email || !password || !confirmPassword) {
-      errors.push({ message: 'All fields are required.' })
-    }
-
-    if (password !== confirmPassword) {
-      errors.push({ message: 'Password and confirm password not the same.' })
-    }
-
-    if (errors.length) {
-      return res.render('register', { name, email, password, confirmPassword, errors })
-    }
-
     const user = await User.findOne({ where: { email } })
     if (user) {
       errors.push({ message: 'User is already exist, please use login feature.' })
       return res.render('register', { name, email, password, confirmPassword, errors })
     }
 
-    await User.create({ name, email, password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)) })
+    await User.create({
+      id: uuidv4(),
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    })
     req.flash('success_message', 'Register successfully.')
     res.redirect('/users/login')
   } catch (error) {
     console.log(error)
   }
+})
+
+router.get('/logout', (req, res) => {
+  req.logout(error => {
+    if (error) return next(error)
+
+    req.flash('success_message', 'Logout successfully.')
+    res.redirect('/users/login')
+  })
 })
 
 module.exports = router
