f5f5a16cadc8da057f0179324a6e2ccbea6a4959
Noelle-KH <noelletang86@gmail.com>
2023-02-22 14:59:35 +0800

    feat: add facebook strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
+const { v4: uuidv4 } = require('uuid')
 const { User } = require('../models')
 
 passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, async (req, email, password, done) => {
@@ -16,6 +18,31 @@ passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true
     }
 
     return done(null, user)
+  } catch (error) {
+    done(error)
+  }
+}))
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
+  callbackURL: process.env.FACEBOOK_CALLBACK,
+  profileFields: ['email', 'displayName']
+}, async (accessToken, refreshToken, profile, done) => {
+  const { name, email } = profile._json
+
+  try {
+    const user = await User.findOne({ where: { email } })
+    if (user) return done(null, user)
+
+    const randomPassword = Math.random().toString(36).slice(-8)
+    const newUser = await User.create({
+      id: uuidv4(),
+      name,
+      email,
+      password: bcrypt.hashSync(randomPassword, bcrypt.genSaltSync(10))
+    })
+    done(null, newUser)
   } catch (error) {
     done(error)
   }
